df81a73037e559ff1e1aa0f188ef4af72153ebe4
Erick Vergara <erick.vergara.ruiz@gmail.com>
2024-10-14 20:30:05 -0500

    feat: grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import './index.css'
+
 function App() {
-  return <></>
+  return (
+    <div id='app' className='relative h-screen p-2 gap-2'>
+      <aside className='[grid-area:aside] flex-col flex overflow-y-auto bg-blue-100'>
+        ASIDE
+      </aside>
+      <main className='[grid-area:main] rounded-lg bg-zinc-900 overflow-y-auto bg-red-200'>
+        MAIN
+      </main>
+      <footer className='[grid-area:player] min-h-[100px]'>PLAYER</footer>
+    </div>
+  )
 }
 
 export default App
